f6b694208d4116cbb02c80abe49148743ad91ecc
Alex LaFroscia <alex@lafroscia.com>
2023-03-14 20:40:28 +0000

    ensure the `browser` condition is loaded

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		
+		// Ensure the `browser` condition is used to load `svelte`
+		{
+			name: 'vitest-resolve-browser-condition',
+			config({ resolve }) {
+				resolve.conditions?.unshift('browser');
+			}
+		}
+	],
 
 	test: {
 		environment: 'jsdom'
